b950d8ac10063e7e01b8ad8455ea308260979e7f
Sebastian Jung <sebi.iuvenis@gmail.com>
2023-01-21 12:21:05 +0100

    add template

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let time = new Date()
+  let time2 = time.toLocaleTimeString()
+  res.render('index', { time: time2 });
 });
 
 module.exports = router;
